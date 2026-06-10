1a89e349a04ad5da0110be20180afc3941cf04bc
kernix01 <sweetyvip4@gmail.com>
2026-06-10 17:37:03 +0500

    JS

---
+++ let_const.js
@@ -0,0 +1,26 @@
+// Пеоеменые
+// let можно изменять, const нет, null значения нет, undefined коробка есть, но пусиая
+// числа без "", строка с "текст"
+// typeof 42 "number"
+// typeof "привет" string это текст
+// true/false boolean
+// typeof undefined просто undefined
+// + тянет к строке, - * / тянет к числу
+// === если с "" то false без true, == всегда true даже при ""
+// typeof название типа 
+
+
+// let ageNumber = 16;
+// let ageText = "52";
+// let user = null;
+// let x;
+// const name = "Viktor";
+
+
+// console.log(ageNumber + 1);
+// console.log(Number(ageText) + 1);
+// console.log(user);
+// console.log(x);
+// console.log(name);
+
+
