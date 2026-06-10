3a540613c1ab9b7c38b7be320591d0f049652fa9
kernix01 <sweetyvip4@gmail.com>
2026-06-10 17:38:09 +0500

    JS

---
+++ else_if.js
@@ -0,0 +1,14 @@
+// let age = 16;
+
+// if (age >= 18) {
+//     console.log("Взрослый");
+// } else if (age >= 14) {
+//     console.log("Подросток");
+// } else {
+//     console.log("Ребенок");
+// }
+
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
+console.log( 5 > 3 && 2 > 10);
